c3f9a0c69dcd1cac5f038d394283379c70fe1093
Rob MacKay <robskiwarrior@gmail.com>
2019-01-24 14:07:00 +0000

    Ok...

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import video from '../img/video.mp4'
 
-export default function Video() {
-  
-    let thevideo = window.document.getElementById('myVideo'); 
-    thevideo.addEventListener('DOMContentLoaded',function(){ video.play(); },false);
-  
+export default class Video extends Component {
 
-  return (
-    <video playsinline autoplay muted loop id="myVideo">
+  videoplay() {
+    let thevideo = window.document.getElementById('myVideo'); 
+    window.document.addEventListener('DOMContentLoaded',function(){ thevideo.play(); },false);
+  }
+
+  componentDidMount() {
+    this.videoplay()
+  }
+
+  render() {
+    return (
+      <video playsinline autoplay muted loop id="myVideo">
         <source src={video} type="video/mp4" />
-    </video>
-  )
+      </video>
+    )
+  }
 }
