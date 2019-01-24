12338435a72646b07b38c8de1e18792faeaeaff4
Rob MacKay <robskiwarrior@gmail.com>
2019-01-24 12:15:53 +0000

    maybe making it play

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,8 +3,11 @@ import video from '../img/video.mp4'
 
 export default function Video() {
   return (
-    <video playsinline autoplay="true" muted="true" loop="true" id="myVideo">
+    <video playsinline autoplay muted loop id="myVideo">
         <source src={video} type="video/mp4" />
     </video>
   )
 }
+
+var video = document.getElementById('myVideo'); 
+video.addEventListener('DOMContentLoaded',function(){ video.play(); },false);
