996a43425f589a8561f7c8b2095fcf18ba38f482
Rob MacKay <robskiwarrior@gmail.com>
2019-01-24 12:21:01 +0000

    test

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,12 +2,13 @@ import React from 'react'
 import video from '../img/video.mp4'
 
 export default function Video() {
+  
+  let thevideo = document.getElementById('myVideo'); 
+  thevideo.addEventListener('DOMContentLoaded',function(){ video.play(); },false);
+  
   return (
     <video playsinline autoplay muted loop id="myVideo">
         <source src={video} type="video/mp4" />
     </video>
   )
 }
-
-var video = document.getElementById('myVideo'); 
-video.addEventListener('DOMContentLoaded',function(){ video.play(); },false);
