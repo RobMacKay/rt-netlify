ba225137253159f457ae57d5dbda38af8327c6a8
Rob MacKay <robskiwarrior@gmail.com>
2019-01-27 11:27:04 +0000

    Getting rid of some images and adding icons

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,21 +1,24 @@
 import React from 'react'
 //import Icon from './Icon'
 import Form from './Form'
+import network from "../img/steps/network.svg"
+import heads from "../img/steps/heads.svg"
+import graph from "../img/steps/graph.svg"
 
 export default function Intro() {
   return (
-    <section className="section">
+    <section className="section intro">
     <div className="container">
       <div className="columns">
         <div className="column is-half">
           <ul>
-            <li>Choose your vertical and the markets you would like to cover</li>
-            <li>Provide the number of inactive players you would like to reactivate</li>
-            <li>Explore the Reactivation rates we can provide</li>
+            <li className="is-size-2 is-flex"><div className="text">Choose your vertical and the markets you would like to cover</div><div className="icon"> <img src={network} alt="Markets" /></div></li>
+            <li className="is-size-2 is-flex"><div className="text">Provide the number of inactive players you would like to reactivate</div><div className="icon"> <img src={heads} alt="Players" /></div></li>
+            <li className="is-size-2 is-flex"><div className="text">Explore the Reactivation rates we can provide</div><div className="icon"> <img src={graph} alt="Rates" /></div></li>
           </ul>
         </div>
         <div className="column is-half">
-        <h1 className="is-uppercase">we know how to communicate with your players</h1>
+        <h1 className="is-uppercase is-size-1">we know how to communicate with your players</h1>
         <Form />
         </div>
       </div>
